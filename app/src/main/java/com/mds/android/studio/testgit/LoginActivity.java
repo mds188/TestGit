b5213559ce322b1747a7cfde1a3ac525106d2e67
mingdasen <mingdasen@icloud.com>
2015-11-05 20:34:44 +0800

    login

---
+++ app/src/main/java/com/mds/android/studio/testgit/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.mds.android.studio.testgit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mingdasen on 2015/11/5.
+ */
+public class LoginActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
