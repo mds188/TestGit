5fd44953c9fccd13fc3bd1b87fedae4341ffa516
mds188 <mingdasen@icloud.com>
2015-11-05 20:58:33 +0800

    Update LoginActivity.java
    add init()

--- app/src/main/java/com/mds/android/studio/testgit/LoginActivity.java
+++ app/src/main/java/com/mds/android/studio/testgit/LoginActivity.java
@@ -10,5 +10,10 @@ public class LoginActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+    }
+    
+    private void init(){
+        
     }
 }
